f304ee16a41c40e120a00e35ef0ef19288d35b2c
admin <admin@MacBook-Pro-von-admin.local>
2026-05-16 20:15:14 +0200

    Part 2 step 1: RegexTokenizer with Task 1 delimiter pattern, casefolding, top-level import

--- Task2/src/part2_02_tokenizer.py
+++ Task2/src/part2_02_tokenizer.py
@@ -1,6 +1,16 @@
-# Tokenizer transformer setup
+# RegexTokenizer using Task 1 delimiter pattern -- splits on whitespace, digits,
+# and punctuation ()[]{}.!?,;:+=-_\"'`~#@&*%$\/. Applies casefolding.
+from pyspark.ml.feature import RegexTokenizer
+from common import TOKEN_DELIMITER_PATTERN, FIELD_REVIEW_TEXT
 
-def create_tokenizer():
-    from pyspark.ml.feature import RegexTokenizer
-    # TODO: configure tokenizer with delimiters
-    pass
+def create_tokenizer(
+    input_col: str = FIELD_REVIEW_TEXT,
+    output_col: str = "tokens",
+):
+    return RegexTokenizer(
+        pattern=TOKEN_DELIMITER_PATTERN,
+        gaps=True,
+        inputCol=input_col,
+        outputCol=output_col,
+        toLowercase=True,
+    )
